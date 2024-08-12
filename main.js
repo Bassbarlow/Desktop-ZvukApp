9f451e783a7b57f1b0f7dfec94b89670c9c824d5
Bassbarlow <max.kochetilov@gmail.com>
2024-08-12 12:32:11 +0300

    show existing process if already running

--- main.js
+++ main.js
@@ -1,15 +1,16 @@
 const { app, BrowserWindow, Menu, Tray } = require('electron');
-const { updateElectronApp } = require('update-electron-app');
+const { updateElectronApp, UpdateSourceType } = require('update-electron-app');
 
 updateElectronApp(); // additional configuration options available
 
 app.on('before-quit', function () {
   isQuiting = true;
 });
-
+let win = null
+const gotTheLock = app.requestSingleInstanceLock()
 const createWindow = () => {
   console.log(process.env.CERT_PASS);
-  const win = new BrowserWindow({
+  win = new BrowserWindow({
     width: 1200,
     height: 800,
     title: 'ZvukApp',
@@ -21,11 +22,11 @@ const createWindow = () => {
   });
 
   win.loadURL('https://zvuk.com/');
-  win.reload();
   win.on('minimize',function(event){
     event.preventDefault();
     win.hide();
 });
+
 
 const tray = new Tray('./music-player.png');
 
@@ -55,18 +56,32 @@ tray.on('double-click', function(e){
 
 }
 
-app.whenReady().then(() => {
-  createWindow()
-
-  app.on('activate', () => {
-    if (BrowserWindow.getAllWindows().length === 0) {
-      createWindow()
+if (!gotTheLock) {
+  app.quit()
+} else {
+  app.on('second-instance', (event, commandLine, workingDirectory) => {
+    // Someone tried to start a second instance, focus our window.
+    if (win) {
+      if (win.isMinimized()) win.restore()
+      win.focus()
     }
   })
-})
+
+  // Create myWindow, load the rest of the application, etc.
+  app.whenReady().then(() => {
+    createWindow()
+  
+    app.on('activate', () => {
+      if (BrowserWindow.getAllWindows().length === 0) {
+        createWindow()
+      }
+    })
+  })
+}
 
 app.on('window-all-closed', () => {
   if (process.platform !== 'darwin') {
     app.quit()
   }
 })
+
